313a8acc387bd4fb67d8ba3cb11bf30cea394f2e
kankuro12 <cms111000111@gmail.com>
2021-01-18 05:10:33 +0545

    css file name changed

--- resources/views/themes/needtech/layout.blade.php
+++ resources/views/themes/needtech/layout.blade.php
@@ -14,7 +14,7 @@
     {{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> --}}
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
     <link rel="stylesheet" type="text/css" href="{{asset('assets/public/css/main1.css')}}">
-    <link rel="stylesheet" type="text/css" href="{{asset('assets/public/css/mobilemenu_V1.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{asset('assets/public/css/mobilemenu_v1.css')}}">
     <link rel="stylesheet" href="{{asset('assets\public\css\sidemenu.css')}}">
     <link rel="stylesheet" href="{{asset('assets\public\css\collection.css')}}">
     <link rel="stylesheet" href="{{asset('assets\public\css\mobile.search.css')}}">
