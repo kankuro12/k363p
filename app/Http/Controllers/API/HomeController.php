f4ee23728142d042de56c61f5ed12e715ea05b2e
kankuro12 <cms111000111@gmail.com>
2021-03-21 08:51:50 +0545

    home page controller finished

--- app/Http/Controllers/API/HomeController.php
+++ app/Http/Controllers/API/HomeController.php
@@ -7,6 +7,7 @@ use App\Model\Vendor\Collection;
 use App\Model\Vendor\Location;
 use App\Model\Vendor\Review;
 use App\Model\Vendor\Room;
+use App\Model\Vendor\RoomPhoto;
 use App\Model\Vendor\RoomType;
 use App\Model\Vendor\Vendor;
 use Illuminate\Http\Request;
@@ -20,13 +21,20 @@ class HomeController extends Controller
         foreach ($fvs as $key => $value) {
             $value->reviewcount=Review::where('vendor_id',$value->id)->count();
             $value->servicecount=Room::where('vendor_id',$value->id)->count();
-            $value->location=Location::where('vendor_id',$value->id)->first();
+            $value->location=Location::where('vendor_id',$value->id)->select('name')->first()->name;
             array_push($featured_vendors,$value);
         }
         // foreach ($variable as $key => $value) {
         //     # code...
         // }
-        $trs = Room::inRandomOrder()->limit(8)->get();
+        $trs = Room::join('room_types','rooms.roomtype_id','=','room_types.id')->join('vendors','rooms.vendor_id','=','vendors.id')->select('rooms.id','rooms.name','rooms.price','rooms.discount',DB::raw('room_types.name as roomtype'),DB::raw('vendors.name as vendor'))->inRandomOrder()->limit(8)->get();
+        $featured_services=[];
+        foreach ($trs  as  $value) {
+            $value->newprice=$value->getNewPrice();
+            $value->images=RoomPhoto::where('room_id',$value->id)->pluck('image');
+            array_push($featured_services,$value);
+        }
+
         $roomtypes=RoomType::all();
         // $cities=City::take(5)->get();
         $popular_vendors = Vendor::leftJoin('reviews', 'reviews.vendor_id', '=', 'vendors.id')
@@ -41,8 +49,9 @@ class HomeController extends Controller
         $collections = Collection::where('status', 1)->get();
         return response()->json([
             'status'=>true,
-            'featured_vendors'=>$fvs,
-            'rooms'=>$trs,
+            'featured_vendors'=>$featured_vendors,
+            'services'=>$featured_services,
+            'packagetypes'=>$roomtypes,
             'popular_vendors'=>$popular_vendors,
             'collections'=>$collections
         ]);
