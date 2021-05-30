efd4476e25722b22448e8f53f6c6551c265af3fd
kankuro12 <cms111000111@gmail.com>
2021-05-30 19:00:34 +0545

    null managed v3

--- resources/views/themes/needtech/snippets/header.blade.php
+++ resources/views/themes/needtech/snippets/header.blade.php
@@ -59,7 +59,7 @@
   <div class="secondary-header sticky" style="display:none;" data-semi="1">
       <div class="menublock">
           <span class="menubtn">
-              <a class="brand" href="/"><img src="{{asset(custom_config('logo')->value)}}"></a>
+              <a class="brand" href="/"><img src="{{asset(custom_config('logo')->value??"")}}"></a>
           </span>
           <span class="mini-search">
               <form action="{{route('n.search')}}">
@@ -126,7 +126,7 @@
   <div class="secondary-header sticky" data-semi="0">
     <div class="menublock">
         <span class="menubtn">
-            <a class="brand" href="/"><img src="{{asset(custom_config('logo')->value)}}"></a>
+            <a class="brand" href="/"><img src="{{asset(custom_config('logo')->value??"")}}"></a>
         </span>
         <span class="mini-search">
             <form action="{{route('n.search')}}">
